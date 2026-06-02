e761737ccdbcd655eb161ec5a0e3bd8252aa3f5f
IAMYANTO <justyanto03@gmail.com>
2026-06-02 12:36:44 +0700

    Update server.py

--- server.py
+++ server.py
@@ -248,4 +248,4 @@ if __name__ == "__main__":
     print("🚀 SERVER AI BERJALAN DI MODE PRODUKSI (WAITRESS) 🚀")
     print("====================================================")
     # 🚨 SUNTIKAN: Buka 4 Kasir sekaligus biar gempuran 2 pintu gak bikin antre/error
-    serve(app, host="0.0.0.0", port=5001, threads=4)
+    serve(app, host="0.0.0.0", port=5000, threads=4)
